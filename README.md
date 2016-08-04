c97743543e4a78b5f3b8792292f45313281fa1a4
Chris Lewis <Bonsitm@gmail.com>
2016-08-04 14:04:53 +0100

    Add documentation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # pebble-packet
+
 Simple wrapper for AppMessage integer and string types with built in error reporting.
+
+## How to use
+
+1. Ensure `AppMessage` is open:
+
+  ```c
+  const int inbox = 128;
+  const int outbox = 128;
+  app_message_open(inbox, outbox);
+  ```
+
+2. Begin, build, and send a packet:
+
+  ```c
+  if(packet_begin()) {
+    packet_put_integer(AppKeyInteger, 42);
+    packet_put_string(AppKeyString, "Don't talk to me about life.");
+    packet_send();
+  }
+  ```
+
+## Documentation
+
+See `include/pebble-packet.h` for function documentation.
+
+## TODO
+
+- `AppMessageOutboxFailed` handling.
+- Some kind of 'is there a packet in progress?' boolean check.
+- Some kind of 'is `AppMessage` open?' check.
--- include/pebble-packet.h
+++ include/pebble-packet.h
@@ -2,11 +2,30 @@
 
 #include <pebble.h>
 
+// Begin a new packet. Must be called before putting anything in it.
+// Returns:
+//   bool - true if the packet was successfully initialised, false otherwise.
+//          Check app logs to see any error reason.
 bool packet_begin();
 
+// Put an integer into the packet.
+// Parameters:
+//   key   - The tuple's key.
+//   value - The tuple's integer value.
+// Returns:
+//   bool - true if the integer was written to the packet, false otherwise.
 bool packet_put_integer(int key, int value);
 
+// Put a string into the packet.
+// Parameters:
+//   key    - The tuple's key.
+//   string - The tuple's string value.
+// Returns:
+//   bool - true if the string was written to the packet, false otherwise.
 bool packet_put_string(int key, char *string);
 
+// Send the packet.
+// Returns:
+//   bool - true if the packet was successfully sent, false otherwise.
+//          Check app logs to see any error reason.
 bool packet_send();
-
